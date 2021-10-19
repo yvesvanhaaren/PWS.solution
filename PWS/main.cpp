a471019b6cb19343e5a45d66e74016f3a3095cf8
Casper Algera <20727@marianum.nl>
2021-10-19 11:56:39 +0200

    Start on calculating coverage

--- PWS/main.cpp
+++ PWS/main.cpp
@@ -1,5 +1,8 @@
 #include "SDL.h"
 
+void SaveScreenshot(SDL_Surface* surface);          //Declar function so we can use them in main
+Uint32 getpixel(SDL_Surface* surface, int x, int y);
+
 int main(int argc, char* argv[])
 {
 
@@ -16,17 +19,47 @@ int main(int argc, char* argv[])
 
         SDL_RenderClear(renderer);  //Draw that colour across the entire screen
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 200);  //Set the colour to white
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 50);  //Set the colour to white
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-        float size = 100;
-        for (float phi = 0; phi < 360; phi++)
+        float accuracy = 2;
+        float size = 250;
+
+        for (float phi = 0; phi < 360; phi += (1/accuracy))
         {
             SDL_RenderDrawLine(renderer, 300, 200, cos(phi) * size + 300, sin(phi) * size + 200);
         }
 
         SDL_RenderPresent(renderer);    //Render the above
 
-        while (SDL_PollEvent(&event)) { //This while loop
+        //Save screen data
+        const Uint32 format = SDL_PIXELFORMAT_ARGB8888;
+        const int width = 640;
+        const int height = 400;
+
+        SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormat(0, width, height, 32, format);
+        SDL_RenderReadPixels(renderer, NULL, format, surface->pixels, surface->pitch);      //Change this null to a rect to calculate for different rooms
+
+
+        SaveScreenshot(surface); //Save a screenshot with the name "name"
+       
+        //Calculate coverage
+        float coverage = 0;
+
+        for (int i = 1; i < surface->w; i++)
+        {
+            for  (int j = 1; j < surface->h; j++) //Loop door alle pixels
+            {
+                SDL_Color rgb;
+                Uint32 data = getpixel(surface, i, j);
+                SDL_GetRGB(data, surface->format, &rgb.r, &rgb.g, &rgb.b);
+                coverage = (coverage * ((i * j - 1) / (i * j))) + ((1) / (i * j)) * (rgb.r + rgb.b + rgb.g) * (1 / 3);
+            }
+        }
+
+        SDL_Delay(3000);
+
+        while (SDL_PollEvent(&event)) { //This while loop ??? doet iets niet weghalen
             if (event.type == SDL_QUIT) {
                 done = SDL_TRUE;
             }
@@ -34,6 +67,47 @@ int main(int argc, char* argv[])
     }
 
 
-
 	return 0;
 }
+
+Uint32 getpixel(SDL_Surface* surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch (bpp)
+    {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16*)p;
+        break;
+
+    case 3:
+        if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32*)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+void SaveScreenshot(SDL_Surface* surface) {
+    //Save a screenshot
+    if (SDL_SaveBMP(surface, "name.bmp") != 0)
+    {
+        // Error saving bitmap
+        SDL_Log("SDL_SaveBMP failed: %s\n", SDL_GetError());
+    }
+}
+
